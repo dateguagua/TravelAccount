db3a8bd43006157f67a2de650b3bb6667f1cf1ae
dateguagua <cornhater.1101@gmail.com>
2025-05-27 14:26:50 +0800

    5.27

--- project/src/main/java/com/example/demo/account/model/entity/Category.java
+++ project/src/main/java/com/example/demo/account/model/entity/Category.java
@@ -1,6 +1,6 @@
 package com.example.demo.account.model.entity;
 
-import java.io.Console;
+
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
@@ -10,37 +10,23 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "accounts")
-public class Account {
+@Data
+@Table(name = "Category")
+public class Category {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-
-	private Long id;
-	
-	@Column(name = "name")
-	private String name;
-	
-	@Column(name = "expense")
-	private Boolean expense;
-	
-	@Column(name = "category")
+	private Integer CategoryId;
 	private String category;
 	
-	@Column(name = "amount")
-	private Long amount;
-	
-	@Column(name = "createDate")
-	private LocalDateTime createDate;
 	
 
 }
