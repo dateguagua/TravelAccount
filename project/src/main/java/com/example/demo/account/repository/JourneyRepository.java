59c22bc3d94ee0d8a5061683b36e583f92b5aa21
dateguagua <cornhater.1101@gmail.com>
2025-05-28 13:58:14 +0800

    5/28

---
+++ project/src/main/java/com/example/demo/account/repository/JourneyRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.account.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.account.model.entity.Category;
+import com.example.demo.account.model.entity.CountryPlan;
+
+@Repository
+public interface JouneyRepository extends JpaRepository<CountryPlan, Integer>{
+	
+	//取得旅行計劃
+	@Query(value = "select country_plan_id, user_id, country_id, start_time, total_days from country where country_id =:countryId", nativeQuery = true)
+	List<Object[]> getCountry(@Param("countryId") Integer countryId);
+
+}
